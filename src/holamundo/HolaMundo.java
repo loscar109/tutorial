62cfa0177726c919257fed1981b1fb7860e2a14d
Nusa <nusamathe@gmail.com>
2017-03-15 06:41:45 -0300

    Agregamos mas comentarios y dos 3 lineas de codigo

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -6,8 +6,10 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo, eh nacido\n");
+		System.out.println("Soy otro comentario mas\n");
+		System.out.println("Somos 2 comentarios en total\n");
     }
 	
-	//esto se guardará en Test
+	//nueva modificacion
     
 }
